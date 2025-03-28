77154bf6858017c19ad5c3dcb01b62f11e607dd6
Reitumetse Marline Mosehla <remosehjhb024@wethinkcode.co.za>
2025-03-28 21:06:11 +0200

    test if position are equal

--- src/test/java/PositionTest.java
+++ src/test/java/PositionTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 import za.co.wethinkcode.examples.toyrobot.Position;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class PositionTest{
     @Test
@@ -9,5 +10,12 @@ public class PositionTest{
         Position p = new Position(10, 20);
         assertEquals(10, p.getX());
         assertEquals(20, p.getY());
+    }
+    @Test
+    public void equality() {
+        assertEquals(new Position(-44, 63), new Position(-44, 63));
+        assertNotEquals(new Position(-44, 63), new Position(0, 63));
+        assertNotEquals(new Position(-44, 63), new Position(-44, 0));
+        assertNotEquals(new Position(-44, 63), new Position(0, 0));
     }
 }
